5f73b2e3027578375ae52fd78403dca65ec6662e
minzion <aert12435@gmail.com>
2024-06-13 17:51:27 +0900

    entity config

--- src/main/kotlin/com/minzino/portfolio/domain/entity/ExperienceDetail.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/entity/ExperienceDetail.kt
@@ -3,13 +3,24 @@ package com.minzino.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ExperienceDetail : BaseEntity() {
+class ExperienceDetail(
+    content: String,
+    isActive: Boolean
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_detail_id")
     val id: Long? = null;
 
-    
+    var content: String = content
+
+    var isActive: Boolean = isActive
+
+    fun update(content: String, isActive: Boolean) {
+
+        this.content = content
+        this.isActive = isActive
+    }
 
 
 }
--- src/main/kotlin/com/minzino/portfolio/domain/entity/ProjectSkill.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/entity/ProjectSkill.kt
@@ -3,13 +3,18 @@ package com.minzino.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ProjectSkill : BaseEntity() {
+class ProjectSkill(project: Project,skill: Skill) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_skill_id")
     val id: Long? = null;
 
-    
+    @ManyToOne(targetEntity = Project::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "projcet_id", nullable = false)
+    var project:Project= project
+    @ManyToOne(targetEntity = Skill::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id", nullable = false)
+    var skill:Skill = skill
 
 
 }
