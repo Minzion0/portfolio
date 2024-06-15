c49926ac48bcff574f9ce80fa35faeee2ca2ebf0
minzion <aert12435@gmail.com>
2024-06-15 22:57:39 +0900

    Repository 개발

--- src/main/kotlin/com/minzino/portfolio/domain/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/presentation/repository/PresentationRepository.kt
@@ -1,8 +1,40 @@
 package com.minzino.portfolio.domain.presentation.repository
 
+import com.minzino.portfolio.domain.entity.*
+import com.minzino.portfolio.domain.repository.*
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository(){
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val skillRepository: SkillRepository,
+    private val projectRepository: ProjectRepository,
+    private val experienceRepository: ExperienceRepository
+) {
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperience(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroduction(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLink(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkill(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProject(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
 }
