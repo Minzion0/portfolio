62412211b0ea6d5a30f3e91a5b8ef7bb3499684a
minzion <aert12435@gmail.com>
2024-06-20 14:13:58 +0900

    controller

--- src/main/kotlin/com/minzino/portfolio/domain/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/presentation/controller/PresentationApiController.kt
@@ -1,5 +1,10 @@
 package com.minzino.portfolio.domain.presentation.controller
 
+import com.minzino.portfolio.domain.presentation.dto.IntroductionDTO
+import com.minzino.portfolio.domain.presentation.dto.LinkDTO
+import com.minzino.portfolio.domain.presentation.dto.ProjectDTO
+import com.minzino.portfolio.domain.presentation.dto.ResumeDTO
+import com.minzino.portfolio.domain.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,11 +12,34 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
+
 
     @GetMapping("/test")
 
-    fun test():String{
+    fun test(): String {
         return "ok"
+    }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
     }
 }
--- src/main/kotlin/com/minzino/portfolio/domain/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/presentation/controller/PresentationViewController.kt
@@ -1,16 +1,48 @@
 package com.minzino.portfolio.domain.presentation.controller
 
+import com.minzino.portfolio.domain.constant.SkillType
+import com.minzino.portfolio.domain.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @Controller
-class PresentationViewController {
+class PresentationViewController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
-    fun test():String{
+    fun test(): String {
         return "test"
+    }
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        val introductions = presentationService.getIntroductions()
+        model.addAttribute("introductions", introductions)
+
+        val links = presentationService.getLinks()
+        model.addAttribute("links", links)
+
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model): String {
+        val resumeDTO = presentationService.getResume()
+        model.addAttribute("resume", resumeDTO)
+        model.addAttribute("skillTypes", SkillType.values())
+
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model): String {
+        val projects = presentationService.getProjects()
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
     }
 
 
