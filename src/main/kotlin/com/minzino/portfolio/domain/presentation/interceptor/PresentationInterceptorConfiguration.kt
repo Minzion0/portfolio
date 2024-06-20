160b0869476bf7d5a193843902041113711c5a61
minzion <aert12435@gmail.com>
2024-06-20 23:40:43 +0900

    인터셉터 개발

--- src/main/kotlin/com/minzino/portfolio/domain/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -2,8 +2,18 @@ package com.minzino.portfolio.domain.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration {
-
+class PresentationInterceptorConfiguration(
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            //모든 경로에 대한 인터셉터 지정
+            .addPathPatterns("/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/admin/**","h2**",
+                "/favicon.ico","/error")
+    }
 }
