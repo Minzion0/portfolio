ebd9df062fc8b1de12f31a4f3745f1b77c8da9bb
minzion <aert12435@gmail.com>
2024-06-21 12:49:14 +0900

    Exception Advice

--- src/main/kotlin/com/minzino/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/minzino/portfolio/admin/exception/AdminException.kt
@@ -5,6 +5,18 @@ import org.springframework.http.HttpStatus
 abstract class AdminException(
     httpStatus: HttpStatus,
     message: String
-) :RuntimeException(message){
-    val httpStatus : HttpStatus=httpStatus
+) : RuntimeException(message) {
+    val httpStatus: HttpStatus = httpStatus
 }
+
+class AdminBadRequestException(message: String) : AdminException(
+    httpStatus = HttpStatus.BAD_REQUEST,
+    message = message
+) {
+
+}
+
+class AdminInternalServerErrorException(message: String) : AdminException(
+    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = message
+) {}
