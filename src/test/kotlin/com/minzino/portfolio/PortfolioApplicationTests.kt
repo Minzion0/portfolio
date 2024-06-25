bc164138e028de496c40276a82758d83986ece21
minzion <aert12435@gmail.com>
2024-06-25 11:57:40 +0900

    최종수정

--- src/test/kotlin/com/minzino/portfolio/PortfolioApplicationTests.kt
+++ src/test/kotlin/com/minzino/portfolio/PortfolioApplicationTests.kt
@@ -12,35 +12,35 @@ class PortfolioApplicationTests {
     @Test
     fun contextLoads() {
     }
-
-    @Test
-    fun jasypt() {
-        val plainText = "8338"
-        val encryptor = PooledPBEStringEncryptor()
-        val config = SimpleStringPBEConfig()
-        config.password = "q1w2e3"
-        config.algorithm = "PBEWithMD5AndDES"
-        config.setKeyObtentionIterations("1000")
-        config.setPoolSize("1")
-        config.providerName = "SunJCE"
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator")
-        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator")
-        config.stringOutputType = "base64"
-        encryptor.setConfig(config)
-        val encryptedText: String = encryptor.encrypt(plainText)
-        val decryptedText: String = encryptor.decrypt(encryptedText)
-
-        println(encryptedText)
-        println(decryptedText)
-    }
-
-    @Test
-    fun test(){
-    //given
-        val encode = BCryptPasswordEncoder().encode("8338")
-        //when
-        println(encode)
-
-    //then
-    }
+//
+//    @Test
+//    fun jasypt() {
+//        val plainText = "8338"
+//        val encryptor = PooledPBEStringEncryptor()
+//        val config = SimpleStringPBEConfig()
+//        config.password = "q1w2e3"
+//        config.algorithm = "PBEWithMD5AndDES"
+//        config.setKeyObtentionIterations("1000")
+//        config.setPoolSize("1")
+//        config.providerName = "SunJCE"
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator")
+//        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator")
+//        config.stringOutputType = "base64"
+//        encryptor.setConfig(config)
+//        val encryptedText: String = encryptor.encrypt(plainText)
+//        val decryptedText: String = encryptor.decrypt(encryptedText)
+//
+//        println(encryptedText)
+//        println(decryptedText)
+//    }
+//
+//    @Test
+//    fun test(){
+//    //given
+//        val encode = BCryptPasswordEncoder().encode("8338")
+//        //when
+//        println(encode)
+//
+//    //then
+//    }
 }
