074ef58999950350c0042a179f39359bde638091
minzion <aert12435@gmail.com>
2024-07-26 23:14:05 +0900

    추가 주석 작성

--- src/main/kotlin/com/minzino/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/minzino/portfolio/domain/entity/Achievement.kt
@@ -5,18 +5,20 @@ import java.time.LocalDate
 
 @Entity
 class Achievement(
-    //놀랍게도 생성자
+    // 생성자 파라미터로 전달된 값들을 프로퍼티로 변환합니다.
     title: String,
     description: String,
     achievedDate: LocalDate?,
     host: String,
     isActive: Boolean
 ) : BaseEntity() {
+    // 기본 키 설정 및 자동 생성 전략을 지정합니다.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null
 
+    // 생성자 파라미터로 전달된 값을 초기화합니다.
     var title: String = title
     var description: String = description
     var achievedDate: LocalDate? = achievedDate
