ded9b1756edd6b15f5b1c7d8db76ebb8b69e7d12
minzion <aert12435@gmail.com>
2024-07-27 23:51:33 +0900

    추가작업

--- src/main/kotlin/com/minzino/portfolio/admin/context/introduction/controller/AdminIntroductionApiController.kt
+++ src/main/kotlin/com/minzino/portfolio/admin/context/introduction/controller/AdminIntroductionApiController.kt
@@ -1,7 +1,5 @@
 package com.minzino.portfolio.admin.context.introduction.controller
 
-import com.minzino.portfolio.admin.context.achievement.form.AchievementForm
-import com.minzino.portfolio.admin.context.achievement.service.AdminAchievementService
 import com.minzino.portfolio.admin.context.introduction.form.IntroductionForm
 import com.minzino.portfolio.admin.context.introduction.service.AdminIntroductionService
 import com.minzino.portfolio.admin.data.ApiResponse
@@ -15,18 +13,17 @@ import org.springframework.web.bind.annotation.*
 class AdminIntroductionApiController(
     private val adminIntroductionService: AdminIntroductionService
 ) {
+    // 새로운 소개글을 등록하는 엔드포인트
     @PostMapping
     fun postAchievement(@RequestBody @Validated form: IntroductionForm): ResponseEntity<Any> {
         adminIntroductionService.save(form)
-
         return ApiResponse.successCreate()
     }
 
+    // 기존 소개글을 수정하는 엔드포인트
     @PutMapping("/{id}")
-    fun putAchievement(@RequestBody form: IntroductionForm, @PathVariable id:Long): ResponseEntity<Any> {
-        adminIntroductionService.update(id,form)
-
+    fun putAchievement(@RequestBody form: IntroductionForm, @PathVariable id: Long): ResponseEntity<Any> {
+        adminIntroductionService.update(id, form)
         return ApiResponse.successUpdate()
-
     }
 }
